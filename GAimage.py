fa9dfc639ddc0f5b86554876cafcc55c585b8b40
Gary William Flake <gary@flake.org>
2018-06-07 20:30:08 -0700

    first working version

--- GAimage.py
+++ GAimage.py
@@ -1,13 +1,17 @@
 import pygame, pygame.gfxdraw
-import math, random
+import numpy, math, random
 
-width = 100
-height = 100
+width = 200
+height = 200
 radius = max(width, height) / 2
-popSize = 100
-geneLen = 50
+
+numGroups = 10
+numShapes = 200
+numCoords = 7
+
 population = None
 running = True
+canvas = None
 
 ################################################################################
 
@@ -21,7 +25,7 @@ def init_graphics():
 ################################################################################
 
 def init_target():
-    global target
+    global target, targetArray
     image = pygame.image.load("daisy.jpg")
     tw = image.get_width()
     th = image.get_height()
@@ -36,11 +40,12 @@ def init_target():
     target = pygame.Surface((width, height)).convert()
     target.fill((0, 0, 0,255))
     target.blit(image, (xoff, yoff))
+    targetArray = numpy.fromstring(target.get_view('0').raw, dtype=numpy.int8)
 
 ################################################################################
 
 def random_circle():
-    return tuple(map(lambda x: random.random(), [None] * 7))    
+    return list(map(lambda x: random.random(), [None] * numCoords))    
 
 ################################################################################
 
@@ -48,9 +53,9 @@ def init_population():
     global population
     #random.seed(0)    
     population = []
-    for i in range(popSize):
+    for i in range(numGroups):
         genes = []
-        for j in range(geneLen):
+        for j in range(numShapes):
             genes.append(random_circle())
         population.append(genes)
 
@@ -78,30 +83,24 @@ def draw_circle(circle, surface):
 
 def render_image(index):
     img = pygame.Surface((width, height))
-    img.fill((0, 0, 0,255))
-    for j in range(geneLen):
+    img.fill((0, 0, 0, 255))
+    for j in range(numShapes):
         draw_circle(population[index][j], img)
     return img
     
 ################################################################################
 
 def compute_image_fitness(img):
-    fitness = 0
-    targetBytes = target.get_view('0').raw
-    imageBytes = img.get_view('0').raw
-    assert(len(targetBytes) == len(imageBytes))
-    for j in range(len(targetBytes)):
-        large = max(imageBytes[j], targetBytes[j])
-        small = min(imageBytes[j], targetBytes[j])
-        fitness += large - small
-    return fitness
+    imageArray = numpy.fromstring(img.get_view('0').raw, dtype=numpy.int8)
+    diff = targetArray - imageArray
+    return abs(diff).sum()
     
 ################################################################################
 
 def compute_population_fitness():
-    sumAll = 0
+    total = 0
     scores = []
-    for i in range(popSize):
+    for i in range(numGroups):
         img = render_image(i)
         score = compute_image_fitness(img)
         scores.append((score, i))
@@ -110,16 +109,48 @@ def compute_population_fitness():
 
 ################################################################################
 
+def mutate_shape(shape):
+    newShape = []
+    for i in range(numCoords):
+        newCoord = shape[i]
+        if (random.random() < 0.01):
+            newCoord += random.random() * 0.1
+            newCoord = max(0, min(1-10E-17, newCoord))
+        newShape.append(newCoord)
+    return newShape
+
+################################################################################
+
+def mutate_group(group):
+    nextGroup = []
+    newOrder = list(range(len(group)))
+    if (random.random() < 0.25):
+        random.shuffle(newOrder)
+    for i in range(len(group)):
+        shape = mutate_shape(group[newOrder[i]])
+        nextGroup.append(shape)
+    return nextGroup
+
+################################################################################
+
 def evolve_population(scores):
-    exit(0)
+    global population
+    nextGen = []
+    numKeep = max(1, int(numGroups / 10))
+    for i in range(numKeep):
+        nextGen.append(population[scores[i][1]])
+    for i in range(numKeep, numGroups):
+        nextGen.append(mutate_group(population[random.randrange(0, numKeep)]))
+    population = nextGen
 
 ################################################################################
 
 def main():
     init()
-    global running
+    global running, canvas
     while running:
         scores = compute_population_fitness()
+        print(scores[0], scores[-1])
         canvas.blit(render_image(scores[0][1]), (0,0))
         evolve_population(scores)
         #
